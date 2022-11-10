d779e31ffe0339ad0010b4646af0a63fe59d9f2a
Joseph Palmezano <josephandres.co@gmail.com>
2022-11-10 05:32:34 -0500

    sign up && sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates_presence_of :email, :full_name, :password, :password_confirmation
+         validates_format_of :email, :with => Devise::email_regexp
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_scope :user do
+    get 'home/index'
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
